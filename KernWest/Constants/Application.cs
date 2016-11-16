8f352dc9de0e44fec9b22eb72d019fc6765985d8
dpark <dpark@DPARKSP4>
2016-11-15 22:36:49 -0800

    Add project files.

---
+++ KernWest/Constants/Application.cs
@@ -0,0 +1,8 @@
+﻿namespace KernWest.Constants
+{
+    public class Application
+    {
+        public const string Name = "Kern West";
+        public const string ShortName = "Boilerplate";
+    }
+}
